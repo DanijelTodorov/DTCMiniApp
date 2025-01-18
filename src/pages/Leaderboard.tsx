fbec58ca4b1b382bb722632f99bebafb12fd2805
Spider <164912081+DanijelTodorov@users.noreply.github.com>
2025-01-18 15:57:01 +0900

    update

--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -1,43 +1,46 @@
 import { CubeButton } from "../components/CubeButton";
 
 export const Leaderboard = () => {
-  const tasks = [
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-    {
-      avatar: "images/avatar.png",
-      task: "Discovery daily ",
-      score: "Rank 01",
-    },
-  ];
+  // const tasks = [
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  //   {
+  //     avatar: "images/avatar.png",
+  //     task: "Discovery daily ",
+  //     score: "Rank 01",
+  //   },
+  // ];
+
+  const tasks: any[] = [];
+
   return (
     <div className="page overflow-x-hidden overflow-y-scroll">
       <img src="images/DTC-bg.png" className="absolute w-full pt-[130px]"></img>
@@ -46,6 +49,24 @@ export const Leaderboard = () => {
           Leader board
         </span>
         <div className="px-[9px] flex flex-col">
+          {tasks.length == 0 && (
+            <div className="flex flex-col items-center gap-2">
+              <span>YOU HAVE NO FRIENDS YET</span>
+              <div className="relative w-full flex justify-center">
+                <CubeButton
+                  className="w-[240px] h-[50px] rounded-[12px] bg-[#003A5C]"
+                  onClick={() => {}}
+                >
+                  <div className="flex items-center justify-center gap-2">
+                    <img src="icons/invite-icon.png"></img>
+                    <span className="text-[20px] leading-[28px] tracking-[2%] text-white">
+                      Invite Friends
+                    </span>
+                  </div>
+                </CubeButton>
+              </div>
+            </div>
+          )}
           {tasks.map((item, index) => (
             <div
               className="flex justify-between items-center py-3 border-b-[1px] border-b-[#6990A7]"
